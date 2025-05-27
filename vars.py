1914c43001dfa9bfd45aaed0fd306c4c94001d3e
Error1234543 <ptaviyad21@gmail.com>
2025-05-27 16:30:44 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "24473318")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7231089830:AAEtg4obq7JHvsZ0TcA-PSBi0vcJABV54e4") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "20619533")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8176277339:AAFwc7xY8cTWyUNVm3Q3_HkyJXTWRyH_kkM") #Replace with your bot token
