c8c647a3f6f32b1015301e4dc9f71ddd6c3e21d2
Error1234543 <ptaviyad21@gmail.com>
2025-10-31 19:45:57 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,4 +5,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20619533")) #Replace with your api id
 API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "8176277339:AAFwc7xY8cTWyUNVm3Q3_HkyJXTWRyH_kkM") #Replace with your bot token
+BOT_TOKEN = environ.get("BOT_TOKEN", "8483889672:AAHF218gaOzYjnoarBrJhNylBmw2C2H65CE") #Replace with your bot token
